74662a03fc4a82f41a4530d8ce26b625b9a716c6
Brian <brian.q.tram@gmail.com>
2024-06-04 19:36:59 -0400

    adjusted code to match current file

--- arduino-recorder/arduino-recorder.ino
+++ arduino-recorder/arduino-recorder.ino
@@ -1,134 +1,153 @@
-const int button1 = D2; 
-const int button2 = D3; 
-const int button3 = D5; 
-const int button4 = D7; 
-const int button5 = D9; 
-const int button6 = D10; 
-const int button7 = D12; 
+//define pins
+const int button1 = D2;
+const int button2 = D3;
+const int button3 = D5;
+const int button4 = D7;
+const int button5 = D9;
+const int button6 = D10;
+const int button7 = D12;
 const int buzzer = D13;
+const int soundSensor = D6;
 
-const int pinButtons[] = {D1, D2, D3, D5, D7, D9, D10, D12}; 
+const int pinButtons[] = { D1, D2, D3, D5, D7, D9, D10, D12 };
 int sensorVal[8];
 bool sensorValPress[8];
 
 const int pinButtonsLeng = sizeof(pinButtons) / sizeof(pinButtons[0]);
 const int sensorValLeng = sizeof(sensorVal) / sizeof(sensorVal[0]);
 
-int noteHighC = 925;
-int noteB = 1025;
+//pwm delays of different notes
+int noteHighC = 950;
+int noteB = 1000;
 int noteA = 1125;
-int noteG = 1225;
-int noteF = 1325;
-int noteE = 1425;
-int noteD = 1525;
-int noteLowC = 1625;
+int noteG = 1250;
+int noteF = 1400;
+int noteE = 1500;
+int noteD = 1675;
+int noteLowC = 1860;
+int loudness = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(buzzer, OUTPUT);
+  pinMode(D6, INPUT);
 
+  //initilizing all buttons as input pullup
   for (int i = 0; i < pinButtonsLeng; i++) {
     pinMode(pinButtons[i], INPUT_PULLUP);
   }
-
 }
 void loop() {
 
-  Serial.println("hello");
+  //read ky-038 digital signal
+  loudness = digitalRead(D6);
 
+  //have a variable set to different button presses
   for (int i = 0; i < sensorValLeng; i++) {
     sensorVal[i] = digitalRead(pinButtons[i]);
   }
 
+  //have a variable to keep track whether a button is pressed
   for (int i = 0; i < sensorValLeng; i++) {
     sensorValPress[i] = (sensorVal[i] == LOW);
   }
 
-  if (sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
-    for(int i = 10; i > 0 ; --i){
+  //code will run when mic picks up loud signal, indicating the blowing of a recorder
+  if (loudness == 1) {
+    //note B
+    if (sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
+      for (int i = 10; i > 0; --i) {
+        digitalWrite(buzzer, HIGH);
+        delayMicroseconds(noteB);
+        digitalWrite(buzzer, LOW);
+        delayMicroseconds(noteB);
+      }
+    } else if (!sensorValPress[1]) {
       digitalWrite(buzzer, HIGH);
-      delayMicroseconds(noteB);
-      digitalWrite(buzzer, LOW);
-      delayMicroseconds(noteB);
     }
-  } else if (!sensorValPress[1]) {
-      digitalWrite (buzzer, HIGH);
-  }
 
-  if (sensorValPress[2] && !sensorValPress[1] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
-    for(int i = 20; i > 0 ; --i){
+    //note high C
+    if (sensorValPress[2] && !sensorValPress[1] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
+      for (int i = 20; i > 0; --i) {
+        digitalWrite(buzzer, HIGH);
+        delayMicroseconds(noteHighC);
+        digitalWrite(buzzer, LOW);
+        delayMicroseconds(noteHighC);
+      }
+    } else if (!sensorValPress[2]) {
       digitalWrite(buzzer, HIGH);
-      delayMicroseconds(noteHighC);
-      digitalWrite(buzzer, LOW);
-      delayMicroseconds(noteHighC);
     }
-  } else if (!sensorValPress[2]) {
-      digitalWrite (buzzer, HIGH);
-  }
 
-  if (sensorValPress[1] && sensorValPress[2] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
-    for(int i = 10; i > 0 ; --i){
+    //note A
+    if (sensorValPress[1] && sensorValPress[2] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
+      for (int i = 10; i > 0; --i) {
+        digitalWrite(buzzer, HIGH);
+        delayMicroseconds(noteA);
+        digitalWrite(buzzer, LOW);
+        delayMicroseconds(noteA);
+      }
+    } else if (!sensorValPress[1] && !sensorValPress[2]) {
       digitalWrite(buzzer, HIGH);
-      delayMicroseconds(noteA);
-      digitalWrite(buzzer, LOW);
-      delayMicroseconds(noteA);
     }
-  } else if (!sensorValPress[1] && !sensorValPress[2]) {
-      digitalWrite (buzzer, HIGH);
-  }
 
-  if (sensorValPress[1] && sensorValPress[2] && sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
-    for(int i = 10; i > 0 ; --i){
+    //note G
+    if (sensorValPress[1] && sensorValPress[2] && sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
+      for (int i = 10; i > 0; --i) {
+        digitalWrite(buzzer, HIGH);
+        delayMicroseconds(noteG);
+        digitalWrite(buzzer, LOW);
+        delayMicroseconds(noteG);
+      }
+    } else if (!sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3]) {
       digitalWrite(buzzer, HIGH);
-      delayMicroseconds(noteG);
-      digitalWrite(buzzer, LOW);
-      delayMicroseconds(noteG);
     }
-  } else if (!sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3]) {
-      digitalWrite (buzzer, HIGH);
-  }
 
-  if (sensorValPress[1] && sensorValPress[2] && sensorValPress[3] && sensorValPress[4] && !sensorValPress[5] && sensorValPress[6] && sensorValPress[7]) {
-    for(int i = 10; i > 0 ; --i){
+    //note F
+    if (sensorValPress[1] && sensorValPress[2] && sensorValPress[3] && sensorValPress[4] && !sensorValPress[5] && sensorValPress[6] && sensorValPress[7]) {
+      for (int i = 10; i > 0; --i) {
+        digitalWrite(buzzer, HIGH);
+        delayMicroseconds(noteF);
+        digitalWrite(buzzer, LOW);
+        delayMicroseconds(noteF);
+      }
+    } else if (!sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress[6] && !sensorValPress[7]) {
       digitalWrite(buzzer, HIGH);
-      delayMicroseconds(noteF);
-      digitalWrite(buzzer, LOW);
-      delayMicroseconds(noteF);
     }
-  } else if (!sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress [6] && !sensorValPress[7]) {
-      digitalWrite (buzzer, HIGH);
-  }
 
-  if (sensorValPress[1] && sensorValPress[2] && sensorValPress[3] && sensorValPress[4] && sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
-    for(int i = 10; i > 0 ; --i){
+    //note E
+    if (sensorValPress[1] && sensorValPress[2] && sensorValPress[3] && sensorValPress[4] && sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
+      for (int i = 10; i > 0; --i) {
+        digitalWrite(buzzer, HIGH);
+        delayMicroseconds(noteE);
+        digitalWrite(buzzer, LOW);
+        delayMicroseconds(noteE);
+      }
+    } else if (!sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5]) {
       digitalWrite(buzzer, HIGH);
-      delayMicroseconds(noteE);
-      digitalWrite(buzzer, LOW);
-      delayMicroseconds(noteE);
     }
-  } else if (!sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3] && !sensorValPress [4] && !sensorValPress[5]) {
-      digitalWrite (buzzer, HIGH);
-  }
 
-  if (sensorValPress[1] && sensorValPress[2] && sensorValPress[3] && sensorValPress[4] && sensorValPress[5] && sensorValPress[6] && !sensorValPress[7]) {
-    for(int i = 10; i > 0 ; --i){
+    //note D
+    if (sensorValPress[1] && sensorValPress[2] && sensorValPress[3] && sensorValPress[4] && sensorValPress[5] && sensorValPress[6] && !sensorValPress[7]) {
+      for (int i = 10; i > 0; --i) {
+        digitalWrite(buzzer, HIGH);
+        delayMicroseconds(noteD);
+        digitalWrite(buzzer, LOW);
+        delayMicroseconds(noteD);
+      }
+    } else if (!sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5] && !sensorValPress[6]) {
       digitalWrite(buzzer, HIGH);
-      delayMicroseconds(noteD);
-      digitalWrite(buzzer, LOW);
-      delayMicroseconds(noteD);
     }
-  } else if (!sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3] && !sensorValPress [4] && !sensorValPress[5] && !sensorValPress[6]) {
-      digitalWrite (buzzer, HIGH);
-  }
 
-  if (sensorValPress[1] && sensorValPress[2] && sensorValPress[3] && sensorValPress[4] && sensorValPress[5] && sensorValPress[6] && sensorValPress[7]) {
-    for(int i = 10; i > 0 ; --i){
+    //note Low C
+    if (sensorValPress[1] && sensorValPress[2] && sensorValPress[3] && sensorValPress[4] && sensorValPress[5] && sensorValPress[6] && sensorValPress[7]) {
+      for (int i = 10; i > 0; --i) {
+        digitalWrite(buzzer, HIGH);
+        delayMicroseconds(noteLowC);
+        digitalWrite(buzzer, LOW);
+        delayMicroseconds(noteLowC);
+      }
+    } else if (!sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3] && !sensorValPress[4] && !sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
       digitalWrite(buzzer, HIGH);
-      delayMicroseconds(noteLowC);
-      digitalWrite(buzzer, LOW);
-      delayMicroseconds(noteLowC);
     }
-  } else if (!sensorValPress[1] && !sensorValPress[2] && !sensorValPress[3] && !sensorValPress [4] && !sensorValPress[5] && !sensorValPress[6] && !sensorValPress[7]) {
-      digitalWrite (buzzer, HIGH);
   }
 }
